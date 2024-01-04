6a8d5e1299c690b69b7a2d67bcb2c26845eb1d21
Dhruv Gandhi <63443072+gandhidhruv2003@users.noreply.github.com>
2024-01-04 09:56:51 +0530

    Add files via upload

---
+++ game.sql
@@ -0,0 +1,13 @@
+create database if not exists game;
+use game;
+
+create table person (
+	person_id varchar(30) primary key,
+    name varchar(255),
+    email varchar(255),
+    phone_number int
+);
+
+create table demography(
+	
+)
