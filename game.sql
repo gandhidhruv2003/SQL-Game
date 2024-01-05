8e7cb3d2a11ee250191cb7ec8d7a3a12a7406dbc
Dhruv Gandhi <63443072+gandhidhruv2003@users.noreply.github.com>
2024-01-05 09:46:05 +0530

    Add files via upload

--- game.sql
+++ game.sql
@@ -2,24 +2,25 @@ create database if not exists game;
 USE game;
 
 create table Person (
-    person_id int primary key,
+    person_id int primary key not null,
     name varchar(255),
     email varchar(255),
-    phone_number varchar(15)
+    phone_number varchar(15),
+    address text
 );
 
 create table Room (
-    room_id int primary key,
+    room_id int primary key not null,
     room_type enum ('Single', 'Double', 'Suite'),
     availability enum ('Available', 'Occupied', 'Under Maintenance'),
-    rate_per_night DECIMAL(10, 2),
+    rate_per_night decimal(10, 2),
     smoking_allowed enum ('Yes', 'No')
 );
 
 create table Guest (
-    guest_id int primary key,
-    person_id int,
-    room_assigned_id int,
+    guest_id int primary key not null,
+    person_id int not null,
+    room_assigned_id int not null,
     check_in date,
     check_out date,
     foreign key (person_id) references Person(person_id),
@@ -27,9 +28,9 @@ create table Guest (
 );
 
 create table Room_Access (
-    access_id int primary key,
-    accesser_id int,
-    room_id int,
+    access_id int primary key not null,
+    accesser_id int not null,
+    room_id int not null,
     check_in_time date,
     check_out_time date,
     foreign key (accesser_id) references Person(person_id),
@@ -37,22 +38,22 @@ create table Room_Access (
 );
 
 create table Department (
-    department_id int primary key,
+    department_id int primary key not null,
     department_name varchar(50)
 );
 
 create table Employee (
-    employee_id int primary key,
-    person_id int,
-    department_id int,
+    employee_id int primary key not null,
+    person_id int not null,
+    department_id int not null,
     joining_date date,
     foreign key (person_id) references Person(person_id),
     foreign key (department_id) references Department(department_id)
 );
 
 create table Demography (
-    demographic_id int primary key,
-    person_id int,
+    demographic_id int primary key not null,
+    person_id int not null,
     gender varchar(10),
     date_of_birth date,
     marital_status enum ('Single', 'Married', 'Divorced'),
@@ -66,8 +67,8 @@ create table Demography (
 );
 
 create table Criminal_Record (
-    record_id int primary key,
-    criminal_id int,
+    record_id int primary key not null,
+    criminal_id int not null,
     crime_type varchar(50),
     crime_date date,
     arress_date date,
@@ -79,7 +80,7 @@ create table Criminal_Record (
 );
 
 create table Crime_Scene_Report (
-    report_id int primary key,
+    report_id int primary key not null,
     date_of_crime date,
     type varchar(50),
     description varchar(255),
@@ -87,6 +88,7 @@ create table Crime_Scene_Report (
 );
 
 create table Interview (
-    InterviewerID int primary key,
-    Transcript TEXT
+    interviewer_id int primary key not null,
+    transcript TEXT,
+    foreign key (interviewer_id) references Person(person_id)
 );
